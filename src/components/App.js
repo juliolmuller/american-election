215c91f48bd50670214057f99287b94df072c0f3
Julio Muller <juliodolago@gmail.com>
2020-11-09 20:52:09 -0300

    Refactored project structure

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.svg'
+
+const App = () => (
+  <div id="app">
+    <div className="container">
+      <img src={logo} className="logo" alt="logo" />
+    </div>
+  </div>
+)
+
+export default App
