0bacb08575442cc69a340b23ffff6f11aa332cd5
Julio Muller <juliodolago@gmail.com>
2020-11-10 00:25:29 -0300

    Fixed class name

--- src/components/CandidatesDeck.js
+++ src/components/CandidatesDeck.js
@@ -1,7 +1,7 @@
 import CandidateCard from './CandidateCard'
 
 const CandidatesDeck = (props) => (
-  <div className="card-deck">
+  <div className="candidates-deck">
     {props.candidates.map((candidate) => (
       <CandidateCard key={candidate.name} {...candidate} />
     ))}
