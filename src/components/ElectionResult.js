6e24b4f4edbc8511bd8b4d502012fd064e6615d2
Julio Muller <juliodolago@gmail.com>
2020-11-12 20:39:49 -0300

    Refined overall styles

--- src/components/ElectionResult.js
+++ src/components/ElectionResult.js
@@ -9,9 +9,15 @@ const ElectionResult = (props) => {
   return (
     <div className="election-result">
       <div className="election-winner">
-        <img src={spinner} className="spinner" alt="spinner" />
-        <img src={rank[0].avatar} className="avatar" alt="avatar do candidato" />
+        <img
+          src={spinner} className="spinner" alt="spinner" />
+        <img
+          src={rank[0].avatar}
+          className="avatar animate__animated animate__bounce animate__repeat-3"
+          alt="avatar do candidato"
+        />
       </div>
+      <h2>{rank[0].name} won!</h2>
       <div className="rank">
         {rank.map((candidate) => (
           <div key={candidate.name} className="rank-item">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import 'animate.css'
 import './assets/styles.css'
 
 ReactDOM.render(
